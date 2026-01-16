e262a6fe76dcb42507238e468430769a631151cc
tulabhagya2 <tulabhagyasri1@gmail.com>
2026-01-16 17:45:48 +0530

    this is crud operations

---
+++ Day-5/server.js
@@ -0,0 +1,88 @@
+import express from "express";
+import fs from "fs";
+import { json } from "stream/consumers";
+const port=5000;
+
+
+const app=express();
+app.use(express.json());
+app.get("/todos",(req,res)=>{
+    let rawData=fs.readFileSync("./db.json","utf-8");
+    //fs always give the data in the form of string
+    console.log("data in the form of string",rawData);
+    let parsedData=JSON.parse(rawData);
+    console.log("parseddata",parsedData);
+    let todos=parsedData.todos;
+
+    res.json({message:"Todos List",data:todos})
+
+});
+
+app.post("/add-todo",(req,res)=>{
+    let rawData=fs.readFileSync("./db.json","utf-8");
+    let parsedData=JSON.parse(rawData);
+    let todos=parsedData.todos;
+    let todoId=todos[todos.length-1].id+1;
+    let newTodo={id:todoId,name:req.body.name,course:req.body.course,year:req.body.year};
+    todos.push(newTodo);
+    let stringifiedData=JSON.stringify(parsedData);
+    fs.writeFileSync("./db.json",stringifiedData)
+    res.json({message:"todo added",data:todos});
+})
+
+app.put("/update-todo/:id", (req, res) => {
+    let id = Number(req.params.id);
+
+    let rawData = fs.readFileSync("./db.json", "utf-8");
+    let parsedData = JSON.parse(rawData);
+
+    let todos = parsedData.todos;
+
+    let index = todos.findIndex(todo => todo.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({ message: "Todo not found" });
+    }
+
+    todos[index] = {
+        ...todos[index],
+        name: req.body.name ?? todos[index].name,
+        course: req.body.course ?? todos[index].course,
+        year: req.body.year ?? todos[index].year
+    };
+
+    fs.writeFileSync("./db.json", JSON.stringify(parsedData, null, 2));
+
+    res.json({
+        message: "Todo updated successfully",
+        data: todos[index]
+    });
+});
+app.delete("/delete-todo/:id", (req, res) => {
+    let id = Number(req.params.id);
+
+    let rawData = fs.readFileSync("./db.json", "utf-8");
+    let parsedData = JSON.parse(rawData);
+
+    let todos = parsedData.todos;
+
+    let index = todos.findIndex(todo => todo.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({ message: "Todo not found" });
+    }
+
+    let deletedTodo = todos.splice(index, 1);
+
+    fs.writeFileSync("./db.json", JSON.stringify(parsedData, null, 2));
+
+    res.json({
+        message: "Todo deleted successfully",
+        data: deletedTodo[0]
+    });
+});
+
+
+app.listen(port,()=>{
+    console.log("server started");
+})
